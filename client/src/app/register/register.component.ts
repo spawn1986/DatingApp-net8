0cec33eb1e440518b20ea903190e3f65ca807221
Daniel Vega <daniel.vega@devsu.com>
2024-07-01 12:18:39 -0500

    End of section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,42 +1,41 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/accounts.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
-  
+  private toastr = inject(ToastrService);
+
   //Input from parent to child component
   //usersFromHomeComponent = input.required<any>();
   //@Input usersFromHomeComponent;  version < 17.3
-  
-  
+
   //Output from child to parent component
   cancelRegister = output<boolean>();
   // @Output cancelRegister = new EventEmitter<boolean>(); version < 17.3
-  
-  
-  
+
   model: any = {};
 
-  register(){
+  register() {
     this.accountService.register(this.model).subscribe({
-      next: response => {
+      next: (response) => {
         console.log(response);
         this.cancel();
       },
-      error: error => console.error(error)
-    })
+      error: (error) => this.toastr.error(error.error),
+    });
   }
-  
-  cancel(){
-    console.log("cancelled");
+
+  cancel() {
+    console.log('cancelled');
     this.cancelRegister.emit(false);
   }
 }

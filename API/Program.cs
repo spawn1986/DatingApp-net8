df9935918685b55aa9decd7dededa7c7ae12c9b7
Daniel Vega <daniel.vega@devsu.com>
2024-07-15 08:52:52 -0500

    End of section 19

--- API/Program.cs
+++ API/Program.cs
@@ -23,10 +23,17 @@ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrig
 app.UseAuthentication();
 app.UseAuthorization();
 
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
 app.MapControllers();
 
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
